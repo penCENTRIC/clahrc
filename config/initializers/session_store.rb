e58f362990c362eb19d43f3d61e17b76fd4774ad
Matthew Gibbons <mhgibbons@me.com>
2010-03-03 13:02:42 +0000

    Added default session config

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,12 @@
-ActionController::Base.session = {
-  :domain      => ENV['SESSION_DOMAIN'],
-  :key         => ENV['SESSION_KEY'],
-  :secret      => ENV['SESSION_SECRET']
-}
+if RAILS_ENV == 'production'
+  ActionController::Base.session = {
+    :domain      => ENV['SESSION_DOMAIN'],
+    :key         => ENV['SESSION_KEY'],
+    :secret      => ENV['SESSION_SECRET']
+  }
+else
+  ActionController::Base.session = {
+    :key         => '_clahrc_session',
+    :secret      => 'c07a714eb4968290551e20b95eb05491f570b2ffb05a07f44dec56ba75d2187cab46db4b41d312da4873adca165caa98b78727a0ccdffa324a342d0a7b28add7'
+  }  
+end
