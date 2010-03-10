4925b1c49d4c7802506ef5264a8f5dc7bfe4bb96
Matthew Gibbons <mhgibbons@me.com>
2010-03-10 11:33:37 +0000

    Added named index for taggable

--- app/concerns/taggable.rb
+++ app/concerns/taggable.rb
@@ -1,6 +1,12 @@
 module Taggable
   def self.included(base)
-    base.acts_as_taggable_on :tags
-    base.attr_accessible :tag_list
+    base.class_eval do
+      acts_as_taggable_on :tags
+      attr_accessible :tag_list
+      
+      define_index "#{base.name.underscore}_taggable" do
+        indexes tags(:name), :as => :tag
+      end
+    end
   end
 end
