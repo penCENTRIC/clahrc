53917ffabd830502ff2e9e1aecdc5d4e41b2c5a5
Matthew Gibbons <mhgibbons@me.com>
2010-07-12 10:49:31 +0100

    Generated new Rails 3 application in place

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = 'c7f7db2a9d274a565527856248d26f20cd5910b51012adbc76f44fe93d5d8cb1aa7c53920ee59222c6a3320b82859ef968e839d753c0fcb9c6b576de0f482996'
--- config/routes.rb
+++ config/routes.rb
@@ -1,122 +1,58 @@
-ActionController::Routing::Routes.draw do |map|
-  # my.clahrc.net
-  map.subdomain :my, :name => nil, :name_prefix => 'my_' do |my|
-    # Settings
-    my.resource :account, :only => [ :show, :edit, :update ]
+Clahrc::Application.routes.draw do |map|
+  # The priority is based upon order of creation:
+  # first created -> highest priority.
 
-    # Profile
-    my.resource :avatar, :only => [ :edit, :update ]
-    my.resource :profile, :only => [ :edit, :update ]
+  # Sample of regular route:
+  #   match 'products/:id' => 'catalog#view'
+  # Keep in mind you can assign values other than :controller and :action
 
-    # Activities
-    my.resources :activities, :as => 'activity', :only => [ :index ], :paged => { :name => :directory }
+  # Sample of named route:
+  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
+  # This route can be invoked with purchase_url(:id => product.id)
 
-    # Assets
-    my.resources :assets, :as => 'files', :paged => { :name => :directory }
+  # Sample resource route (maps HTTP verbs to controller actions automatically):
+  #   resources :products
 
-    # Content
-    my.resources :pages, :collection => { :sort => :put }, :paged => { :name => :directory }
-    my.resources :posts, :paged => { :name => :directory }
-    my.resources :wiki_pages, :as => :wiki
-    
-    # Friendships
-    my.resources :friendships, :as => 'friends', :collection => { :pending => :get }, :member => { :accept => :put, :reject => :delete }, :only => [ :index, :create, :destroy ], :paged => { :name => :directory }
-    
-    # Messages
-    my.resources :sent_messages, :as => :sent, :only => [ :index, :show, :destroy ], :path_prefix => 'messages', :paged => { :name => :directory }
-    my.resources :received_messages, :as => :messages, :only => [ :index, :show, :destroy ], :collection => { :unread => :get }, :member => { :reply => :get }, :paged => { :name => :directory }
+  # Sample resource route with options:
+  #   resources :products do
+  #     member do
+  #       get :short
+  #       post :toggle
+  #     end
+  #
+  #     collection do
+  #       get :sold
+  #     end
+  #   end
 
-    # Memberships
-    my.resources :memberships, :as => 'groups', :collection => { :invited => :get }, :member => { :accept => :put, :reject => :delete }, :only => [ :index, :destroy ], :paged => { :name => :directory }
+  # Sample resource route with sub-resources:
+  #   resources :products do
+  #     resources :comments, :sales
+  #     resource :seller
+  #   end
 
-    # Default route
-    my.root :controller => 'activities', :action => 'index'
-  end
-  
-  # community.clahrc.net
-  map.subdomain :community, :name => nil do |community|
-    community.resources :users, :as => :members, :only => [ :new, :edit, :create, :update ], :path_names => {  :new => 'register', :edit => 'activate' }
-    community.resource :user_session, :as => :session, :only => [ :new, :create, :destroy ], :path_names => { :new => 'login' }
-    community.resources :passwords, :only => [ :new, :edit, :create, :update ], :path_names => { :new => 'reset', :edit => 'reset' }
-    
-    # Members
-    community.resources :members, :only => [ :index, :show ], :collection => { :autocomplete => :get }, :paged => { :name => :directory } do |member|
-      # Activities
-      member.resources :activities, :as => 'activity', :only => [ :index ], :paged => { :name => :directory }
+  # Sample resource route with more complex sub-resources
+  #   resources :products do
+  #     resources :comments
+  #     resources :sales do
+  #       get :recent, :on => :collection
+  #     end
+  #   end
 
-      # Friendships
-      member.resources :friendships, :as => 'friends', :only => [ :index, :create ], :paged => { :name => :directory }
+  # Sample resource route within a namespace:
+  #   namespace :admin do
+  #     # Directs /admin/products/* to Admin::ProductsController
+  #     # (app/controllers/admin/products_controller.rb)
+  #     resources :products
+  #   end
 
-      # Memberships
-      member.resources :memberships, :as => 'groups', :only => [ :index ], :paged => { :name => :directory }
+  # You can have the root of your site routed with "root"
+  # just remember to delete public/index.html.
+  # root :to => "welcome#index"
 
-      # Messages
-      member.resources :messages, :only => [ :new, :create ]
+  # See how all your routes lay out with "rake routes"
 
-      # Assets
-      member.resources :assets, :as => 'files', :only => [ :index ], :collection => { :block => :get }, :paged => { :name => :directory, :index => true }
-      
-      # Content
-      member.resources :pages, :only => [ :index ], :collection => { :block => :get }, :paged => { :name => :directory, :index => true }
-      member.resources :posts, :only => [ :index ], :collection => { :block => :get }, :paged => { :name => :directory, :index => true }
-      member.resources :wiki_pages, :as => :wiki, :only => [ :index, :show ]
-    end
-    
-    # Groups
-    community.resources :groups, :paged => { :name => :directory } do |group|
-      # Activitites
-      group.resources :activities, :as => 'activity', :only => [ :index ], :paged => { :name => :directory }
-
-      # Memberships
-      group.resources :memberships, :as => 'members', :collection => { :pending => :get, :invite => :post }, :member => { :accept => :put, :promote => :put, :reject => :delete }, :only => [ :index, :new, :create ], :paged => { :name => :directory }
-      group.resources :moderatorships, :as => 'moderators', :member => { :promote => :put }, :only => [ :index ], :paged => { :name => :directory }
-      group.resources :ownerships, :as => 'owners', :only => [ :index ], :paged => { :name => :directory }
-
-      # Messages
-      group.resources :messages, :only => [ :new, :create ]
-      
-      # Assets
-      group.resources :assets, :as => 'files', :only => [ :index, :new, :create ], :collection => { :block => :get }, :paged => { :name => :directory, :index => true }
-
-      # Content
-      group.resources :forums, :only => [ :index, :new, :create ], :paged => { :name => :directory }
-      group.resources :pages, :only => [ :index, :new, :create ], :collection => { :block => :get }, :paged => { :name => :directory, :index => true }
-      group.resources :wiki_pages, :as => :wiki
-    end
-
-    # Assets
-    community.resources :assets, :as => 'files', :only => [ :show, :edit, :update, :destroy ]
-    
-    # Forums
-    community.resources :forums, :only => [ :show, :edit, :update, :destroy ] do |forum|
-      forum.resources :topics, :only => [ :index, :new, :create ], :paged => { :name => :directory }
-    end
-
-    # Pages
-    community.resources :pages, :only => [ :show, :edit, :update, :destroy ] do |page|
-      page.resources :comments, :only => [ :index, :new, :create ], :paged => { :name => :directory }
-    end
-    
-    # Posts
-    community.resources :posts, :only => [ :show, :edit, :update, :destroy ] do |post|
-      post.resources :comments, :only => [ :index, :new, :create ], :paged => { :name => :directory }
-    end
-    
-    # Topics
-    community.resources :topics, :only => [ :show, :edit, :update, :destroy ] do |topic|
-      topic.resources :comments, :only => [ :index, :new, :create ], :paged => { :name => :directory }
-    end
-  
-    # Search
-    community.resources :search, :only => [ :index ], :collection => { :forums => :get, :groups => :get, :members => :get, :pages => :get, :posts => :get, :topics => :get, :wiki_pages => :get }
-  
-    # Comments
-    community.resources :comments, :only => [ :show, :edit, :update, :destroy ], :member => { :reply => :get }
-    
-    # Tags
-    community.resources :tags, :only => [ :index, :show ]
-
-    # Default route
-    community.root :controller => 'members', :action => 'index'
-  end
+  # This is a legacy wild controller route that's not recommended for RESTful applications.
+  # Note: This route will make all actions in every controller accessible via GET requests.
+  # match ':controller(/:action(/:id(.:format)))'
 end
