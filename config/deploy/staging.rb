9e6d7d5c68668f89a054645b64e0fad219eecfb8
Matthew Gibbons <mhgibbons@me.com>
2010-07-13 12:28:50 +0100

    Added support for deploying from staging branch

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,6 +2,7 @@ set :domain, "dptserver2.ex.ac.uk"
 set :user, "matthew"
 set :deploy_to, "/var/www/domains/clahrc.net/web/public"
 set :rails_env, "staging"
+set :branch, "staging"
 
 role :app, "#{domain}"
 role :web, "#{domain}"
