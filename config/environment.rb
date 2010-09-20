34b1d43e3574819afc016ee4e7a0b3b99bd05367
James Stewart <james@ketlai.co.uk>
2010-09-20 14:00:19 +0100

    Declare dependency on mysql

--- config/environment.rb
+++ config/environment.rb
@@ -20,6 +20,8 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
+  config.gem 'mysql'
+
   config.gem 'compass'
   config.gem 'gravtastic', :source => 'http://gemcutter.org'
   config.gem 'hoptoad_notifier'
