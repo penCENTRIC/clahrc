a40e467fe46935cf145791d85aaab01e1e177b1c
James Stewart <james@ketlai.co.uk>
2010-10-05 12:37:09 +0100

    Add some very basic (and not hugely helpful) specs for the user model. These were part of an effort to test something that turned out to be easier (for now) to test manually

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,35 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe User do
-  before(:each) do
-    @valid_attributes = {
+  describe "processing notifications" do
+    before(:each) do
+      @new_notification = Notification.new
+      @new_notification.stubs(:save).returns(true)
       
-    }
-  end
-
-  it "should create a new instance given valid attributes" do
-    User.create!(@valid_attributes)
+      @user = User.new
+      @notification_proxy = stub_everything('notifications', :find_or_initialize_by_activity_id => @new_notification)
+      @preferences_proxy = stub_everything('notification preferences')
+      
+      @user.stubs(:notifications).returns(@notification_proxy)
+      @user.stubs(:notification_preferences).returns(@preferences_proxy)
+      
+      @activity = stub('comment activity', :id => 1)
+      @details = { :event => 'new content in group', :status => 'posted', :activity => self }
+    end
+    
+    it "looks to see if a notification has been sent for this activity" do
+      @notification_proxy.expects(:find_or_initialize_by_activity_id).returns(@new_notification)
+      @user.process_notification(@details)
+    end
+    
+    it "looks for a contextual preference if appropriate" do
+      @preferences_proxy.expects(:find_by_context).returns(nil)
+      @user.process_notification(@details, Group.new)
+    end
+    
+    it "saves the resulting notification" do
+      @new_notification.expects(:save).returns(true)
+      @user.process_notification(@details)
+    end
   end
 end
