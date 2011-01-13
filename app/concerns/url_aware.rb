2de014fec10b61bcd2a3a74e79b4e755e424a9a9
James Stewart <jys@ketlai.co.uk>
2011-01-13 22:19:00 +0000

    Allow options to be passed to content_url

--- app/concerns/url_aware.rb
+++ app/concerns/url_aware.rb
@@ -11,21 +11,21 @@ module UrlAware
     end
   end
   
-  def content_url(content_item)
+  def content_url(content_item, options = {})
     case content_item.type
     when 'Page'
-      page_url(content_item)
+      page_url(content_item, options)
     when 'Post'
-      post_url(content_item)
+      post_url(content_item, options)
     when 'Forum'
-      forum_url(content_item)
+      forum_url(content_item, options)
     when 'Topic'
-      topic_url(content_item)
+      topic_url(content_item, options)
     when 'WikiPage'
       if content_item.group
-        group_wiki_page(content_item.group, content_item)
+        group_wiki_page(content_item.group, content_item, options)
       else
-        member_wiki_page(content_item.owner, content_item)
+        member_wiki_page(content_item.owner, content_item, options)
       end
     end
   end
